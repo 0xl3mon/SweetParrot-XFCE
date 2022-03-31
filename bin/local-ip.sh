6d41195e4cc41a17d613002003c5b0aaaac96529
0xl3mon <96775743+0xl3mon@users.noreply.github.com>
2022-03-31 12:15:04 +0200

    Update local-ip.sh

--- bin/local-ip.sh
+++ bin/local-ip.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 #icon
-icon="/home/l3mon/.local/bin/icons/cave.png"
+icon="/home/${USER}/.local/bin/icons/cave.png"
 
 # Ip
-local_ip=$(/sbin/ifconfig | grep "ens[0-9][0-9]" -A 1 | grep inet | awk '{print $2}' | tr -d '\n')
+local_ip=$(/sbin/ifconfig | grep -E "ens[0-9][0-9]?|eth?[0-9]0-9]?" -A 1 | grep inet | awk '{print $2}' | tr -d '\n')
 
 info="<img>${icon}</img>"
 info+="<txt>${local_ip}</txt>"
