78b6c441da2f4325b94b6eb925025040ec30fd2c
0xl3mon <96775743+0xl3mon@users.noreply.github.com>
2022-04-10 15:17:21 +0200

    Update local-ip.sh

--- bin/local-ip.sh
+++ bin/local-ip.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #icon
-icon="/home/${USER}/.local/bin/icons/cave.png"
+icon="/home/${USER}/.local/bin/icons/castle-localhost.png"
 
 # Ip
 local_ip=$(/sbin/ifconfig | grep -E "ens[0-9][0-9]?|eth?[0-9]0-9]?" -A 1 | grep inet | awk '{print $2}' | tr -d '\n')
